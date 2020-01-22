10bd86538249229020da1d2a6bc16fc911db8434
S-Hanin <sign.rx@yandex.ru>
2020-01-22 11:50:52 +0300

    Initial commit

---
+++ task1/task1.py
@@ -0,0 +1,62 @@
+# -*-coding: utf8-*-
+import copy
+from collections import Counter
+
+
+# task1
+def capitalize(text: str) -> str:
+    return " ".join([word.capitalize() for word in text.split()])
+
+
+# task2
+def count_frequency(text: str) -> dict:
+    return Counter(text)
+
+
+# task3
+def cut_middle_chars(text: str) -> str:
+    if len(text) < 2:
+        return ""
+    return f"{text[:2]}{text[-2:]}"
+
+
+# task4
+def count_words(words: list) -> int:
+    return len([word for word in words
+                if len(word) > 2 and word.startswith(word[-1])])
+
+
+# task5
+def is_subset(first: set, second: set, third: set) -> bool:
+    return third.issubset(first) and third.issubset(second)
+
+
+# task6
+def generate_squares(size: int) -> dict:
+    return {x: x * x for x in range(1, size + 1)}
+
+
+# task7
+def merge_dicts(first: dict, second: dict) -> dict:
+    result = copy.deepcopy(first)
+    result.update(second)
+    return result
+
+
+# task8
+def get_highest_three_values(pairs: dict) -> list:
+    if len(pairs) < 3:
+        raise AssertionError("Dictionary must have 3 or more values")
+    values = sorted(pairs.values())
+    return values[-3:]
+
+
+# task9
+def remove_duplicates(collection: list) -> list:
+    return list(set(collection))
+
+
+# task10
+def get_difference(first: list, second: list) -> list:
+    collection = first + second
+    return [item for item in collection if item not in first or item not in second]
---
+++ task1/tests/tests_task1.py
@@ -0,0 +1,51 @@
+# -*-coding: utf8-*-
+
+import unittest
+import task1.task1 as task1
+
+
+class Task1Test(unittest.TestCase):
+
+    def test_task1_first_letter_is_uppercase(self):
+        self.assertEqual("Alison Heck", task1.capitalize("alison heck"))
+
+    def test_task2_character_frequency(self):
+        should_be = {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+        self.assertEqual(should_be, task1.count_frequency("google.com"))
+
+    def test_task3_cut_middle_of_string(self):
+        self.assertEqual("w3ce", task1.cut_middle_chars("w3resource"))
+        self.assertEqual("w3w3", task1.cut_middle_chars("w3"))
+        self.assertEqual("", task1.cut_middle_chars("w"))
+
+    def test_task4_count_strings_where_first_and_last_letters_are_equal(self):
+        self.assertEqual(2, task1.count_words(['abc', 'xyz', 'aba', '1221']))
+        self.assertEqual(0, task1.count_words(['abc', 'xyz']))
+
+    def test_task5_is_subset(self):
+        self.assertTrue(task1.is_subset({1, 2}, {2, 3}, {2}))
+        self.assertFalse(task1.is_subset({1, 2}, {3, 4}, {5}))
+
+    def test_task6_generate_dict(self):
+        should_be = {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+        self.assertEqual(should_be, task1.generate_squares(5))
+
+    def test_task7_merge_dicts(self):
+        should_be = {"a": 1, "b": 2}
+        self.assertEqual(should_be, task1.merge_dicts({"a": 1}, {"b": 2}))
+
+    def test_task8_find_highest_three_values_in_dict(self):
+        should_be = [9, 16, 25]
+        test_data = task1.generate_squares(5)
+        self.assertEqual(should_be, task1.get_highest_three_values(test_data))
+        self.assertRaises(AssertionError,
+                          task1.get_highest_three_values,
+                          task1.generate_squares(2))
+        with self.assertRaises(AssertionError):
+            task1.get_highest_three_values(task1.generate_squares(2))
+
+    def test_task9_remove_duplicates_from_list(self):
+        self.assertEqual([1, 2, 3], task1.remove_duplicates([1, 1, 2, 2, 3]))
+
+    def test_task10_get_difference(self):
+        self.assertEqual([4, 4, 3], task1.get_difference([1, 2, 4, 4], [1, 2, 3]))
