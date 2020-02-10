a66ea579096aa7c49dcf54f6292dd4ef467efc9d
S-Hanin <sign.rx@yandex.ru>
2020-02-10 17:51:06 +0300

    changed cycle

--- hw6/task6_4.py
+++ hw6/task6_4.py
@@ -8,7 +8,7 @@
 from collections.abc import Sequence
 
 
-def cycle(collection: Sequence, repeat: int = 1):
+def cycle(collection: Sequence):
     """
     Returns `LoopedIterator` instance, who iterates collection
     `repeat` times
@@ -17,35 +17,5 @@ def cycle(collection: Sequence, repeat: int = 1):
     :param repeat: repeat count
     :return: LoopedIterator
     """
-
-    class LoopedIterator:
-        """
-        LoopedIterator
-        """
-
-        def __init__(self):
-            self._current_index = -1
-            self._repeat_counter = 0
-            self._collection_length = len(collection)
-
-        def __iter__(self):
-            return self
-
-        def __next__(self):
-            if self._collection_length == 0:
-                raise StopIteration
-            if not self._has_next():
-                self._reset()
-            return collection[self._current_index]
-
-        def _has_next(self):
-            self._current_index += 1
-            return self._current_index < len(collection)
-
-        def _reset(self):
-            self._current_index = 0
-            self._repeat_counter += 1
-            if self._repeat_counter == repeat:
-                raise StopIteration
-
-    return LoopedIterator()
+    while True:
+        yield from (item for item in collection)
--- hw6/tests/test_task6_4.py
+++ hw6/tests/test_task6_4.py
@@ -3,15 +3,23 @@ import unittest
 from task6_4 import cycle
 
 
+def limit_cycle(collection, limit):
+    result = []
+    g = cycle(collection)
+    for item in range(len(collection) * limit):
+        result.append(next(g))
+    return result
+
+
 class Task64Test(unittest.TestCase):
     def test_returns_elements_n_times(self):
         expected = [1, 2, 1, 2]
-        actual = list(cycle([1, 2], 2))
+        actual = list(limit_cycle([1, 2], 2))
         self.assertListEqual(expected, actual)
 
     def test_correct_work_with_empty_collections(self):
         expected = []
-        actual = list(cycle([], 2))
+        actual = list(limit_cycle([], 2))
         self.assertListEqual(expected, actual)
 
 
