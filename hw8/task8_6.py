e2c9f41ee158f336ffd762ac72a3765f53486b4d
S-Hanin <sign.rx@yandex.ru>
2020-02-17 17:34:27 +0300

    create task8_6.py

--- hw8/task8_6.py
+++ hw8/task8_6.py
@@ -5,3 +5,20 @@
 в которых одно слово (более 2 символов)
 повторяется 4 или более раз.
 """
+import re
+
+
+def is_queston_has_duplicated_words(text: str):
+    """
+    Checks sentence contains word duplicated four and more times
+    Doesn't work with cyrillic
+    """
+    rx = r".*(\w{2,})(.*?\1){3,}.*\?$"
+    return True if re.search(rx, text) else False
+
+
+if __name__ == '__main__':
+    TEXT = "how much how to to to had you been saying and how" \
+           " to speak so silly things like you?"
+    print(TEXT)
+    print(f"has duplicated words: {is_queston_has_duplicated_words(TEXT)}")
