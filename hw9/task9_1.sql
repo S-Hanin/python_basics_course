43996e9d4d086674c4814848c3e83f24768c5a3d
S-Hanin <sign.rx@yandex.ru>
2020-02-20 17:59:19 +0300

    create task9_1.sql

---
+++ hw9/task9_1.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS `staff` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `surname` varchar(64) NOT NULL,
+  `salary` decimal(10,2) unsigned NOT NULL,
+  `position` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO staff (name, surname, salary, position) VALUES ("Артем", "Дзюба", 25000, "менеджер по продажам");
+INSERT INTO staff (name, surname, salary, position) VALUES ("Игорь", "Акинфеев", 25000, "менеджер по продажам");
+INSERT INTO staff (name, surname, salary, position) VALUES ("Павел", "Крашенинников", 30000, "менеджер по продажам");
+INSERT INTO staff (name, surname, salary, position) VALUES ("Антон", "Михайленко", 50000, "руководитель отдела продаж");
+INSERT INTO staff (name, surname, salary, position) VALUES ("Мария", "Васина", 20000, "офисменеджер");
+
+
+
+SELECT * FROM staff WHERE salary < 30000;
+SELECT * FROM staff WHERE position = "менеджер по продажам" AND salary < 30000;
+
+
+CREATE TABLE `staff_rel` (
+  `chief_id` int(10) unsigned NOT NULL,
+  `employee_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`chief_id`,`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO staff_rel (chief_id, employee_id) VALUES (4, 1), (4, 2), (4, 3);
+
+
+SELECT s.name, s.surname, s.salary, s.position FROM staff s
+INNER JOIN staff_rel sr ON s.id=sr.employee_id
+WHERE sr.chief_id = (SELECT id FROM staff WHERE position="руководитель отдела продаж");
