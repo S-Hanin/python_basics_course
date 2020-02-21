da683c40fa11f235791081c066d3b5f1a8a388fa
S-Hanin <sign.rx@yandex.ru>
2020-02-21 10:36:19 +0300

    columns rename

--- hw9/task9_1.sql
+++ hw9/task9_1.sql
@@ -1,7 +1,10 @@
+create database if not exists artezio_hw9;
+use artezio_hw9;
+
 CREATE TABLE IF NOT EXISTS `staff` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  `surname` varchar(64) NOT NULL,
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(64) NOT NULL,
   `salary` decimal(10,2) unsigned NOT NULL,
   `position` varchar(64) NOT NULL,
   PRIMARY KEY (`id`),
@@ -9,11 +12,11 @@ CREATE TABLE IF NOT EXISTS `staff` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-INSERT INTO staff (name, surname, salary, position) VALUES ("Артем", "Дзюба", 25000, "менеджер по продажам");
-INSERT INTO staff (name, surname, salary, position) VALUES ("Игорь", "Акинфеев", 25000, "менеджер по продажам");
-INSERT INTO staff (name, surname, salary, position) VALUES ("Павел", "Крашенинников", 30000, "менеджер по продажам");
-INSERT INTO staff (name, surname, salary, position) VALUES ("Антон", "Михайленко", 50000, "руководитель отдела продаж");
-INSERT INTO staff (name, surname, salary, position) VALUES ("Мария", "Васина", 20000, "офисменеджер");
+INSERT INTO staff (first_name, last_name, salary, position) VALUES ("Артем", "Дзюба", 25000, "менеджер по продажам");
+INSERT INTO staff (first_name, last_name, salary, position) VALUES ("Игорь", "Акинфеев", 25000, "менеджер по продажам");
+INSERT INTO staff (first_name, last_name, salary, position) VALUES ("Павел", "Крашенинников", 30000, "менеджер по продажам");
+INSERT INTO staff (first_name, last_name, salary, position) VALUES ("Антон", "Михайленко", 50000, "руководитель отдела продаж");
+INSERT INTO staff (first_name, last_name, salary, position) VALUES ("Мария", "Васина", 20000, "офисменеджер");
 
 
 
@@ -31,6 +34,6 @@ CREATE TABLE `staff_rel` (
 INSERT INTO staff_rel (chief_id, employee_id) VALUES (4, 1), (4, 2), (4, 3);
 
 
-SELECT s.name, s.surname, s.salary, s.position FROM staff s
+SELECT s.first_name, s.last_name, s.salary, s.position FROM staff s
 INNER JOIN staff_rel sr ON s.id=sr.employee_id
 WHERE sr.chief_id = (SELECT id FROM staff WHERE position="руководитель отдела продаж");
